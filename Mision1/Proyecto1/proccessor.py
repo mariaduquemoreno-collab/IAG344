ba11503bcc293e8ebec09b9d8f1cdebec85dea20
mariaduquemoreno-collab <mariaduquemoreno@gmail.com>
2026-01-17 10:00:46 -0500

    trabajostalentotech

--- Mision1/Proyecto1/proccessor.py
+++ Mision1/Proyecto1/proccessor.py
@@ -1,6 +1,24 @@
 import re 
 def clean_id(value):
+    # funcion clean id
     #elimina caracteres no numericos de un documento
     if value is None:
         return ""
     return re.sub(r'\D','',str(value))
+
+#funcion merge_name
+#un nombre y apellido en un solo campo
+def merge_name(name,lastname):
+    if name is None:
+        name = ""
+    if lastname is None:
+        lastname = ""
+    return f"{name}{lastname}" .strip()
+
+from proccessor import clean_id,merge_name
+def test_clean_id():
+    assert clean_id("cc-75.087.345")
+
+ 
+
+
